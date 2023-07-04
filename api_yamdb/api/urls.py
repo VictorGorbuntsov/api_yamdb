7334b8607927949be6584a7ad671854d199c7814
Виктор Горбунцов <gorbuntsov_v@mail.ru>
2023-07-04 14:10:17 +0300

    Добавлены эндпоинты на категории, жанры, тайтлы добавлен urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,3 +8,6 @@ router = SimpleRouter()
 router.register(r'categories', views.CategoryViewSet, basename='categories')
 router.register(r'genres', views.GenreViewSet, basename='genres')
 router.register(r'titles', views.TitleViewSet, basename='titles')
+
+
+
